b712b15f11ef64351a65cca062087bc26ecfd3b8
Peter Eshak Abdo <mixcolor11@gmail.com>
2026-07-31 02:28:49 +0300

    add superAdmin to db-connect file

--- Day19/config/db-connect.js
+++ Day19/config/db-connect.js
@@ -1,9 +1,21 @@
 const mongoose = require("mongoose");
+const User = require("../model/user");
 
 const mongoConnect = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Connect To MongoDB")
+    console.log("Connect To MongoDB");
+    const superAdmin = await User.findOne({
+      email: process.env.SUPERADMIN_EMAIL,
+    });
+    if (!superAdmin) {
+      await User.create({
+        username: process.env.SUPERADMIN_NAME,
+        email: process.env.SUPERADMIN_EMAIL,
+        password: process.env.SUPERADMIN_PASSWORD,
+        role: "super-admin",
+      });
+    }
   } catch (error) {
     console.log("Failed toConnect to MongoDB", error)
   }
